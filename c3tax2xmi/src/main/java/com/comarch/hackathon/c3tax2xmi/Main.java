265cb1bb13493920bbddde5481269876a7c97059
Marcin Koch <marcin.koch.pl@gmail.com>
2016-04-05 09:26:46 +0200

    Hello hackathon

--- c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
+++ c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
@@ -3,8 +3,7 @@ package com.comarch.hackathon.c3tax2xmi;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello Hackathon");
 	}
 
 }
