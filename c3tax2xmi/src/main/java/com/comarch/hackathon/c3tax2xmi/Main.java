4ae8d6970aec9927a56f8e508ec80fe6397e56e8
Cezariusz Marek <cezariusz.marek@comarch.pl>
2016-04-05 11:52:22 +0200

    Odblokowane wywołanie kodu Pawła.

--- c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
+++ c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
@@ -6,34 +6,36 @@ import java.io.File;
 
 public class Main {
 
-
 	public static void main(String[] args) {
 		System.out.println("hello Hackathon");
 		boolean genXmi = false;
-        for (String arg : args) {
-            switch (arg) {
-                case "--genxmi":
-                    genXmi = true;
-                    break;
-                default:
-                    System.err.println("Invalid parameter: " + arg);
-            }
-        }
-        if (genXmi) {
-        	XmiGenerator gen = new XmiGenerator();
-        	gen.generate();
-        }
-		
+		boolean loadRdf = false;
+		for (String arg : args) {
+			switch (arg) {
+			case "--genxmi":
+				genXmi = true;
+				break;
+			case "--loadrdf":
+				loadRdf = true;
+				break;
+			default:
+				System.err.println("Invalid parameter: " + arg);
+			}
+		}
+		if (genXmi) {
+			XmiGenerator gen = new XmiGenerator();
+			gen.generate();
+		}
 
-
-        
-        /*try {
-            C3TaxParser parser = new C3TaxParser();
-            parser.parse(new File("d:\\Projekty\\Hackathon\\Codding Challange\\em_short.rdf"));
-            parser.getParsedElements();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }*/
-    }
+		if (loadRdf) {
+			try {
+				C3TaxParser parser = new C3TaxParser();
+				parser.parse(new File("d:\\Projekty\\Hackathon\\Codding Challange\\em_short.rdf"));
+				parser.getParsedElements();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 
 }
