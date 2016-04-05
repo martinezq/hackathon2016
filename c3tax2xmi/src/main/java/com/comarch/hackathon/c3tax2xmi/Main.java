1bf2008d313dd57468ed6d4091490e86b2ebba8e
Cezariusz Marek <cezariusz.marek@comarch.pl>
2016-04-05 16:17:57 +0200

    Usunięty stary generator

--- c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
+++ c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
@@ -1,6 +1,6 @@
 package com.comarch.hackathon.c3tax2xmi;
 
-import com.comarch.hackathon.c3tax2xmi.xmi.XmiGenerator;
+import com.comarch.hackathon.c3tax2xmi.xmi.StaxXmiGenerator;
 import com.comarch.hackathon.saxparser.C3TaxParser;
 import java.io.File;
 
@@ -10,32 +10,54 @@ public class Main {
 		System.out.println("hello Hackathon");
 		boolean genXmi = false;
 		boolean loadRdf = false;
-		for (String arg : args) {
+		String rdf = null;
+		String xmi = "out.xmi";
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
 			switch (arg) {
 			case "--genxmi":
 				genXmi = true;
 				break;
 			case "--loadrdf":
-				loadRdf = true;
+				if (i == args.length - 1) {
+					System.err.println("Missing parameter for --loadrdf");
+				} else {
+					loadRdf = true;
+					genXmi = true;
+					i++;
+					rdf = args[i];
+				}
+				break;
+			case "--xmi":
+				if (i == args.length - 1) {
+					System.err.println("Missing parameter for --xmi");
+				} else {
+					genXmi = true;
+					i++;
+					xmi = args[i];
+				}
 				break;
 			default:
 				System.err.println("Invalid parameter: " + arg);
 			}
 		}
-		if (genXmi) {
-			XmiGenerator gen = new XmiGenerator();
-			gen.generate();
-		}
-
 		if (loadRdf) {
+			File file = new File(rdf);
+			if (!file.exists()) {
+				System.err.println("Cannot find RDF file " + rdf);
+				return;
+			}
 			try {
 				C3TaxParser parser = new C3TaxParser();
-				parser.parse(new File("d:\\Projekty\\Hackathon\\Codding Challange\\em_short.rdf"));
-				parser.getParsedElements();
+				parser.parse(file);
+				//parser.getParsedElements();
+				StaxXmiGenerator generator = new StaxXmiGenerator(parser.getParsedElements());
+				generator.write(xmi);				
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
+
 	}
 
 }
