94a55d923d6393edd5fe20349bf229154dbdf2fc
Cezariusz Marek <cezariusz.marek@comarch.pl>
2016-04-05 11:34:56 +0200

    Obsługa XMI.

--- c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
+++ c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/Main.java
@@ -1,9 +1,26 @@
 package com.comarch.hackathon.c3tax2xmi;
 
+import com.comarch.hackathon.c3tax2xmi.xmi.XmiGenerator;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello Hackathon");
+		boolean genXmi = false;
+        for (String arg : args) {
+            switch (arg) {
+                case "--genxmi":
+                    genXmi = true;
+                    break;
+                default:
+                    System.err.println("Invalid parameter: " + arg);
+            }
+        }
+        if (genXmi) {
+        	XmiGenerator gen = new XmiGenerator();
+        	gen.generate();
+        }
+		
 	}
 
 }
