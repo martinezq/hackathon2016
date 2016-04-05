e10cbd158ed6791f96ba83b92fc3cffd41cddd29
Marcin Koch <marcin.koch.pl@gmail.com>
2016-04-05 15:08:42 +0200

    stax based XMI exporter

---
+++ c3tax2xmi/src/main/java/com/comarch/hackathon/c3tax2xmi/xmi/StaxXmiGenerator.java
@@ -0,0 +1,131 @@
+package com.comarch.hackathon.c3tax2xmi.xmi;
+
+import java.io.FileWriter;
+import java.util.List;
+import java.util.UUID;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import com.comarch.hackathon.saxparser.model.RdfSubject;
+
+public class StaxXmiGenerator {
+
+	static String xmiNs = "http://schema.omg.org/spec/XMI/2.1";
+	static String umlNs = "http://schema.omg.org/spec/UML/2.1";
+	
+	XMLStreamWriter writer;
+	
+	List<RdfSubject> subjects;
+
+	String modelId = UUID.randomUUID().toString();
+	
+	public StaxXmiGenerator(List<RdfSubject> subjects) {
+		this.subjects = subjects;
+	}
+
+	public void write(String file) {
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+
+			XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newInstance();
+			writer = xmlOutputFactory.createXMLStreamWriter(fileWriter);
+			
+			writeHeaderStart();
+			writeModelStart();
+			
+			int count = 0;
+			
+			writePackageStart(UUID.randomUUID().toString(), "C3 Taxonomy");
+			
+			for(RdfSubject subject: subjects) {
+				writeClassStart(UUID.randomUUID().toString(), "test" + UUID.randomUUID().toString());
+				writeClassEnd();
+				
+				if(count++ > 1) {
+					break;
+				}
+			}
+			
+			writePackageEnd();
+			
+			writeModelEnd();
+/*
+			writer.writeStartElement("carname");
+			writer.writeAttribute("type", "formula one");
+			writer.writeCharacters("Ferrari 101");
+			writer.writeEndElement();
+
+			writer.writeStartElement("carname");
+			writer.writeAttribute("type", "sports");
+			writer.writeCharacters("Ferrari 202");
+			writer.writeEndElement();
+*/
+			writeHeaderEnd();
+
+			writer.flush();
+			writer.close();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private void writeHeaderStart() throws XMLStreamException {
+		writer.writeStartDocument();
+		
+		writer.writeStartElement("xmi", "XMI", xmiNs);
+		writer.writeNamespace("xmi", xmiNs);
+		writer.writeNamespace("uml", umlNs);
+		writer.writeAttribute(xmiNs, "version", "2.1");
+
+		writer.writeStartElement(xmiNs, "Documentation");
+		writer.writeAttribute("exporter", "Comarch C3Tax2XMI");
+		writer.writeAttribute("exporterVersion", "0.1");
+		writer.writeEndElement();
+	}
+	
+	private void writeHeaderEnd() throws XMLStreamException {
+		writer.writeEndElement();
+		writer.writeEndDocument();
+	}
+	
+	private void writeModelStart() throws XMLStreamException {
+		// <uml:Model xmi:type="uml:Model" xmi:id="themodel" name="sample">
+		writer.writeStartElement(umlNs, "Model");
+		writer.writeAttribute(xmiNs, "type", "uml:Model");
+		writer.writeAttribute(xmiNs, "id", modelId);
+		writer.writeAttribute("name", "C3 Taxonomy Model");
+	}
+	
+	private void writeModelEnd() throws XMLStreamException {
+		writer.writeEndElement();
+	}
+	
+	private void writePackageStart(String id, String name) throws XMLStreamException {
+		//<packagedElement xmi:type="uml:Package" xmi:id="BOUML_0x81_22" name ="sample">
+		writer.writeStartElement("packagedElement");
+		writer.writeAttribute(xmiNs, "type", "uml:Package");
+		writer.writeAttribute(xmiNs, "id", id);
+		writer.writeAttribute("name", name);		
+	}
+	
+	private void writePackageEnd() throws XMLStreamException {
+		writer.writeEndElement();
+	}
+	
+	private void writeClassStart(String id, String name) throws XMLStreamException {
+		//<packagedElement xmi:type="uml:Class" name="Order" xmi:id="BOUML_0x1f498_4" visibility="package" isAbstract="false" >
+		writer.writeStartElement("packagedElement");
+		writer.writeAttribute(xmiNs, "type", "uml:Class");
+		writer.writeAttribute(xmiNs, "id", id);
+		writer.writeAttribute("name", name);
+		writer.writeAttribute("visibility", "package");
+	}
+	
+	private void writeClassEnd() throws XMLStreamException {
+		writer.writeEndElement();
+	}
+	
+}
---
+++ c3tax2xmi/src/test/java/com/comarch/hackathon/c3tax2xmi/xmi/StaxXmiGeneratorTest.java
@@ -0,0 +1,29 @@
+package com.comarch.hackathon.c3tax2xmi.xmi;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.comarch.hackathon.saxparser.C3TaxParser;
+
+public class StaxXmiGeneratorTest {
+
+	@Test
+	public void test() {
+		try {
+			C3TaxParser parser = new C3TaxParser();
+			parser.parse(new File("q:\\hackathon\\eahackathon\\Coding Challange\\em.rdf" ));
+			
+			StaxXmiGenerator generator = new StaxXmiGenerator(parser.getParsedElements());
+			
+			generator.write("out/stax.xml");
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
